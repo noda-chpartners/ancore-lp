e586e6b80eb1025f494e75f93c04dbc32041cb17
Mode-s <shota.umemoto7@gmail.com>
2026-07-22 18:37:21 +0900

    add section

--- src/contents/content.ts
+++ src/contents/content.ts
@@ -1,4 +1,4 @@
-import Logo from '../assets/logo.png'
+import Logo from '../assets/logo.png';
 
 export const siteContent = {
   site: {
@@ -11,14 +11,23 @@ export const siteContent = {
       alt: '株式会社Ancoreロゴ',
     },
     nav: [
-      { label: '求人一覧', href: '#' },
+      { label: '事業内容', href: '#' },
       { label: '仕事内容', href: '#' },
-      { label: '働く環境', href: '#' },
       { label: '募集要項', href: '#' },
       { label: '応募する', href: '#', isButton: true },
     ],
   },
+  business: [
+    { label: 'インサイドセールス・マーケティング業務', description: '' },
+    { label: '新規事業開発支援', description: '' },
+    { label: 'SNS運用・Web制作・動画制作', description: '' },
+    { label: '人材育成・研修', description: '' },
+    { label: '採用支援', description: '' },
+  ],
+  requirement: {},
+  apply: {},
   footer: {
+    sns: [{ label: 'instagram', href: 'https://www.instagram.com/weare_ancore/?hl=ja', icon: 'simple-icons:instagram' }],
     copyright: '2026 株式会社Ancore',
-  }
+  },
 } as const;
